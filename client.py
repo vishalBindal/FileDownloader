2e2e82fc6deb9b50353df8eadfdaff2212427e6c
vishalBindal <vishalbindal.06@gmail.com>
2020-11-17 05:18:12 +0530

    Add try/except to enable download in case of errors, download from multiple servers

--- client.py
+++ client.py
@@ -5,18 +5,20 @@ import os
 import sys
 import threading
 from heapq import heappush, heappop
-import math
+from math import ceil
+import time
 
 output_path = 'output.txt'
 if not os.path.exists(output_path):
     Path(output_path).mkdir(parents=True, exist_ok=True)
 f = open(output_path, 'wb')
 
-N_connections = 5
-Server_host = 'vayu.iitd.ac.in'
+Server_hosts = ['vayu.iitd.ac.in', 'norvig.com']
+N_connections = [6,0]
 Target_path = '/big.txt'
 Chunk_size = 10000
 
+socket.setdefaulttimeout(5)
 Threads = []
 
 class DataQueue:
@@ -38,7 +40,7 @@ class DataQueue:
 
 class TrackChunks:
     def __init__(self, content_length):
-        self.n_chunks = math.ceil(content_length / Chunk_size)
+        self.n_chunks = ceil(content_length / Chunk_size)
         self.lowest_unassigned_chunk = 0
         self.lock = threading.Lock()
 
@@ -91,31 +93,35 @@ def get_chunk_size(header):
             return int(words[1])
     return -1
 
-def create_request(bytes_start, bytes_end, keep_alive=True):
+def create_request(bytes_start, bytes_end, server_name, keep_alive=True):
     alive_message = ''
     if keep_alive:
         alive_message = 'Connection: keep-alive\r\n'
-    req = f'GET {Target_path} HTTP/1.1\r\nHost: {Server_host}\r\n{alive_message}Range: bytes={bytes_start}-{bytes_end}\r\n\r\n'
+    req = f'GET {Target_path} HTTP/1.1\r\nHost: {server_name}\r\n{alive_message}Range: bytes={bytes_start}-{bytes_end}\r\n\r\n'
     return req.encode()
 
-def get_request(chunk_no):
+def get_request(chunk_no, server_name):
     bytes_start = chunk_no*Chunk_size
     bytes_end = bytes_start + Chunk_size - 1
-    return create_request(bytes_start, bytes_end)
+    return create_request(bytes_start, bytes_end, server_name)
 
 # Get content-length
 content_length = -1
 while content_length < 0:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((Server_host, 80))
-    s.sendall(create_request(0,0,False))
-    data = s.recv(4096)
-    header, content = split_header(data)
-    if check_ok(header):
-        content_length = get_content_length(header)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((Server_hosts[0], 80))
+        s.sendall(create_request(0,0,Server_hosts[0],False))
+        data = s.recv(4096)
+        header, content = split_header(data)
+        if check_ok(header):
+            content_length = get_content_length(header)
+    except Exception as e:
+        print('Error in initial request (to obtain content length):', e)
+        time.sleep(1)
 print(f'Content_length: {content_length} bytes')
 
-def socket_task(s, s_id, tracker, dataqueue):
+def socket_task(server_name, s, s_id, tracker, dataqueue):
     while True:
         chunk_no = tracker.get_chunk()
         if chunk_no < 0:
@@ -127,60 +133,66 @@ def socket_task(s, s_id, tracker, dataqueue):
             break
         while True:
             try:
-                s.sendall(get_request(chunk_no))
-            except:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((Server_host, 80))
-                print(f'Socket {s_id} connected to {Server_host}, port 80')
-                s.sendall(get_request(chunk_no))
-            print(f'Socket {s_id} sent a GET request for chunk {chunk_no}')
-            
-            data = s.recv(4096)
-            
-            header, content = split_header(data)
-            if not check_ok(header):
-                print(f'Response on socket {s_id} not OK')
-                # print(header)
-                # print(content)
-                # input()
-                s.close()
-                continue
-            chunk_s = get_chunk_size(header)
-            if chunk_s < 0:
-                print(f'Unable to parse chunk size on socket {s_id}')
-                # print(header)
-                # print(content)
-                # input()
-                s.close()
-                continue
-
-            current_length = len(content)
-            
-            while current_length < chunk_s:
+                try:
+                    s.sendall(get_request(chunk_no, server_name))
+                except:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect((server_name, 80))
+                    print(f'Socket {s_id} connected to {server_name}, port 80')
+                    s.sendall(get_request(chunk_no, server_name))
+                print(f'Socket {s_id} sent a GET request for chunk {chunk_no}')
+                
                 data = s.recv(4096)
-                if not data:
-                    print(f'Chunk on socket {s_id} not received fully. Will request for chunk again.')
-                    break
-                current_length = current_length + len(data)
-                content = content + data
+                
+                header, content = split_header(data)
+                if not check_ok(header):
+                    print(f'Response on socket {s_id} not OK')
+                    # print(header)
+                    # print(content)
+                    # input()
+                    s.close()
+                    continue
+                chunk_s = get_chunk_size(header)
+                if chunk_s < 0:
+                    print(f'Unable to parse chunk size on socket {s_id}')
+                    # print(header)
+                    # print(content)
+                    # input()
+                    s.close()
+                    continue
 
-            if current_length >= chunk_s:
-                print(f'Received chunk {chunk_no} on socket {s_id} fully, and will be written to disk shortly.')
-                x = threading.Thread(target=dataqueue.push, args=(chunk_no, content))
-                x.start()
-                Threads.append(x)
-                break
+                current_length = len(content)
+                
+                while current_length < chunk_s:
+                    data = s.recv(4096)
+                    if not data:
+                        print(f'Chunk on socket {s_id} not received fully. Will request for chunk again.')
+                        break
+                    current_length = current_length + len(data)
+                    content = content + data
+
+                if current_length >= chunk_s:
+                    print(f'Received chunk {chunk_no} on socket {s_id} fully, and will be written to disk shortly.')
+                    x = threading.Thread(target=dataqueue.push, args=(chunk_no, content))
+                    x.start()
+                    Threads.append(x)
+                    break
+            except Exception as e:
+                print(f'Error on socket {s_id} in receiving chunk {chunk_no}: {e}')
+                s.close()
+                time.sleep(2)
 
 tracker = TrackChunks(content_length)
 dataqueue = DataQueue()
 Sockets = []
 
-for i in range(N_connections):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Sockets.append(s)
-    x = threading.Thread(target=socket_task, args=(s, len(Sockets), tracker, dataqueue))
-    x.start()
-    Threads.append(x)
+for j in range(len(Server_hosts)):
+    for i in range(N_connections[j]):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Sockets.append(s)
+        x = threading.Thread(target=socket_task, args=(Server_hosts[j], s, len(Sockets), tracker, dataqueue))
+        x.start()
+        Threads.append(x)
 
 for x in Threads:
     x.join()
